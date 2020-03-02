b5c5fa8b7d2aef4f96468b72d4f4f7ce6b318743
謝長霖 <singularity@Aaron-MBP.local>
2020-03-03 03:14:45 +0800

    commit 5000

--- toSelectCourse.sh
+++ toSelectCourse.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo -e "\n"selectCourse"\n"
-nohup ./selectCourse_Linux.py > ./select/course$i.txt < /dev/null &
-echo $! > ./select/pidCourse$i.txt
+nohup ./selectCourse_Linux.py > ./select/course$1.txt < /dev/null &
+echo $! > ./select/pidCourse$1.txt
